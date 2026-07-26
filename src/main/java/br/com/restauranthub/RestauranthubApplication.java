3c70bd46d22df976cdeae2bb9fb2b54a4b2fe3d3
Felipe Barbosa <felipe.sbarbosadev@gmail.com>
2026-07-26 15:32:46 -0300

    Initial project

---
+++ src/main/java/br/com/restauranthub/RestauranthubApplication.java
@@ -0,0 +1,13 @@
+package br.com.restauranthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranthubApplication.class, args);
+	}
+
+}
